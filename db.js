efbc540570a1d8ab5f637e42407b6251cb0c11c1
Christian Zela <link.zela@gmail.com>
2024-09-18 20:28:11 +0200

    created first design

---
+++ db.js
@@ -0,0 +1,110 @@
+let quiz = [
+    {
+        "questions": [
+            {
+                "question": "Was ist der richtige Weg, um eine Funktion in JavaScript zu definieren?",
+                "answers": [
+                    {
+                        "text": "function meineFunktion() {}",
+                        "isCorrect": true
+                    },
+                    {
+                        "text": "def meineFunktion() {}",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "create function meineFunktion() {}",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "func meineFunktion() {}",
+                        "isCorrect": false
+                    }
+                ]
+            },
+            {
+                "question": "Wie kann man eine Variable in JavaScript deklarieren?",
+                "answers": [
+                    {
+                        "text": "var x = 5;",
+                        "isCorrect": true
+                    },
+                    {
+                        "text": "let x := 5;",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "int x = 5;",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "x == 5;",
+                        "isCorrect": false
+                    }
+                ]
+            },
+            {
+                "question": "Welche der folgenden Methoden stoppt die Ausführung eines Intervalls in JavaScript?",
+                "answers": [
+                    {
+                        "text": "clearInterval()",
+                        "isCorrect": true
+                    },
+                    {
+                        "text": "stopInterval()",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "pauseInterval()",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "terminateInterval()",
+                        "isCorrect": false
+                    }
+                ]
+            },
+            {
+                "question": "Wie konvertiert man eine Zeichenkette in eine Ganzzahl in JavaScript?",
+                "answers": [
+                    {
+                        "text": "parseInt('123')",
+                        "isCorrect": true
+                    },
+                    {
+                        "text": "toInteger('123')",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "Number('123')",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "int('123')",
+                        "isCorrect": false
+                    }
+                ]
+            },
+            {
+                "question": "Wie kann man in JavaScript auf das erste Element eines Arrays zugreifen?",
+                "answers": [
+                    {
+                        "text": "array[0]",
+                        "isCorrect": true
+                    },
+                    {
+                        "text": "array.first()",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "array(1)",
+                        "isCorrect": false
+                    },
+                    {
+                        "text": "array{0}",
+                        "isCorrect": false
+                    }
+                ]
+            }
+        ]
+    }]
